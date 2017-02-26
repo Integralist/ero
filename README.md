c8da08b7f8d74b4e52fd4e6eb33ea12d961f3669
Integralist <mark.mcdx@gmail.com>
2017-02-26 18:57:20 +0000

    Functioning project

--- README.md
+++ README.md
@@ -1,4 +1,77 @@
 # ero
+
 Ero is a [Go](https://golang.org) binary diff checker between local VCL files and those stored in a [Fastly CDN](https://www.fastly.com/) account
 
+The purpose of this binary is to allow you to easily check whether the version of a set of vcl files for a specific service 'version' (in Fastly terminology) is actually what you have in a local repo.
+
+Typically I will be working within a 'staging' service environment (this is where we test out configuration before applying it to our production environment). Lots of different engineers 'borrow' the staging environment so they can test out their files, but they don't necessarily put `master` back. Meaning I don't know what's changed in comparison to the branch I happen to be working on.
+
+This tool allows me to quickly verify which vcl files I need to update, as apposed to me blindly uploading 10+ separate vcl files via the Fastly UI.
+
 > ero is "difference" in Finnish
+
+## Installation
+
+```bash
+go get github.com/integralist/ero
+```
+
+Alternatively you can [download a pre-compiled binary](#)
+
+## Usage
+
+```bash
+ero -help
+
+  -debug
+        show the error/diff output
+  -dir string
+        vcl directory to compare files against (default "VCL_DIRECTORY")
+  -help
+        show available flags
+  -match string
+        regex for matching vcl directories
+  -service string
+        your service id (default: FASTLY_SERVICE_ID) (default "FASTLY_SERVICE_ID")
+  -skip string
+        regex for skipping vcl directories (default "^____")
+  -token string
+        your fastly api token (default: FASTLY_API_TOKEN) (default "FASTLY_API_TOKEN")
+```
+
+Specify credentials via cli flags:
+
+```bash
+ero -service 123abc -token 456def
+```
+
+> If no flags provided, fallback to environment vars:  
+> `FASTLY_SERVICE_ID` and `FASTLY_API_TOKEN`
+
+View the error/diff output using the debug flag:
+
+```bash
+ero -debug
+```
+
+> Typically you'll not care for the output,  
+> you just want to know the files didn't match up
+
+Specify which nested directories you want to verfiy against:
+
+```bash
+ero -match 'foo|bar'
+```
+
+> Note: .git directories are automatically ignored  
+> If no flag provided, we'll look for the environment var:  
+> `VCL_MATCH_DIRECTORY`
+
+Specify which nested directories you want to skip over:
+
+```bash
+ero -skip 'foo|bar'
+```
+
+> If no flag provided, we'll look for the environment var:  
+> `VCL_SKIP_DIRECTORY`
---
+++ main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/fatih/color"
+	fastly "github.com/sethvargo/go-fastly"
+)
+
+// Fastly API doesn't return sorted data
+type version struct {
+	Number  int
+	Version *fastly.Version
+}
+
+type wrappedVersions []version
+
+// Satisfy the Sort interface
+func (v wrappedVersions) Len() int      { return len(v) }
+func (v wrappedVersions) Swap(i, j int) { v[i], v[j] = v[j], v[i] }
+func (v wrappedVersions) Less(i, j int) bool {
+	return v[i].Number < v[j].Number
+}
+
+type vclResponse struct {
+	Path    string
+	Name    string
+	Content string
+}
+
+// Globals needed for sharing between functions
+var fastlyServiceID string
+var latestVersion string
+
+// List of VCL files to process
+var vclFiles []string
+
+// Regex used to define user specific filtering
+var dirSkipRegex *regexp.Regexp
+var dirMatchRegex *regexp.Regexp
+
+// WaitGroup and Channel for storing responses from API
+var wg sync.WaitGroup
+var ch chan vclResponse
+
+func main() {
+	help := flag.Bool("help", false, "show available flags")
+	debug := flag.Bool("debug", false, "show the error/diff output")
+	service := flag.String("service", os.Getenv("FASTLY_SERVICE_ID"), "your service id (fallback: FASTLY_SERVICE_ID)")
+	token := flag.String("token", os.Getenv("FASTLY_API_TOKEN"), "your fastly api token (fallback: FASTLY_API_TOKEN)")
+	dir := flag.String("dir", os.Getenv("VCL_DIRECTORY"), "vcl directory to compare files against")
+	skip := flag.String("skip", "^____", "regex for skipping vcl directories")
+	match := flag.String("match", "", "regex for matching vcl directories")
+	flag.Parse()
+
+	if *help == true {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	envSkipDir := os.Getenv("VCL_SKIP_DIRECTORY")
+	envMatchDir := os.Getenv("VCL_MATCH_DIRECTORY")
+
+	if envSkipDir != "" {
+		*skip = envSkipDir
+	}
+
+	if envMatchDir != "" {
+		*match = envMatchDir
+	}
+
+	dirSkipRegex, _ = regexp.Compile(*skip)
+	dirMatchRegex, _ = regexp.Compile(*match)
+
+	client, err := fastly.NewClient(*token)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fastlyServiceID = *service
+
+	listVersions, err := client.ListVersions(&fastly.ListVersionsInput{
+		Service: fastlyServiceID,
+	})
+
+	wv := wrappedVersions{}
+	for _, v := range listVersions {
+		i, err := strconv.Atoi(v.Number)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		wv = append(wv, version{i, v})
+	}
+	sort.Sort(wv)
+
+	latestVersion = strconv.Itoa(wv[len(wv)-1].Number)
+
+	walkError := filepath.Walk(*dir, aggregate)
+	if walkError != nil {
+		fmt.Printf("filepath.Walk() returned an error: %v\n", walkError)
+	}
+
+	ch = make(chan vclResponse, len(vclFiles))
+	for _, vclPath := range vclFiles {
+		wg.Add(1)
+		go getVCL(vclPath, client)
+	}
+	wg.Wait()
+	close(ch)
+
+	for vclFile := range ch {
+		processDiff(vclFile, *debug)
+	}
+}
+
+func aggregate(path string, f os.FileInfo, err error) error {
+	if validPathDefaults(path) && validPathUserDefined(path) && !invalidPathUserDefined(path) {
+		vclFiles = append(vclFiles, path)
+	}
+
+	return nil
+}
+
+func validPathDefaults(path string) bool {
+	return !strings.Contains(path, ".git") && strings.Contains(path, ".vcl")
+}
+
+func validPathUserDefined(path string) bool {
+	return dirMatchRegex.MatchString(path)
+}
+
+func invalidPathUserDefined(path string) bool {
+	return dirSkipRegex.MatchString(path)
+}
+
+func getVCL(path string, client *fastly.Client) {
+	defer wg.Done()
+
+	name := extractName(path)
+
+	vclFile, err := client.GetVCL(&fastly.GetVCLInput{
+		Service: fastlyServiceID,
+		Version: latestVersion,
+		Name:    name,
+	})
+
+	if err != nil {
+		ch <- vclResponse{
+			Path:    path,
+			Name:    name,
+			Content: "error",
+		}
+	} else {
+		ch <- vclResponse{
+			Path:    path,
+			Name:    name,
+			Content: vclFile.Content,
+		}
+	}
+}
+
+func extractName(path string) string {
+	_, file := filepath.Split(path)
+	return strings.Split(file, ".")[0]
+}
+
+func processDiff(vr vclResponse, debug bool) {
+	var (
+		err    error
+		cmdOut []byte
+	)
+	cmdName := "diff"
+	cmdArgs := []string{"-w", "-B", "-I", "[[:space:]]\\+#", "-", vr.Path}
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmd.Stdin = strings.NewReader(vr.Content)
+
+	if cmdOut, err = cmd.Output(); err != nil {
+		color.Red("\nThere was a difference between the version (%s) of '%s' and the version found locally\n\t%s\n", latestVersion, vr.Name, vr.Path)
+
+		if debug == true {
+			fmt.Printf("\n%s\n", string(cmdOut))
+		}
+	} else {
+		color.Green("\nNo difference between the version (%s) of '%s' and the version found locally\n\t%s\n", latestVersion, vr.Name, vr.Path)
+	}
+}
